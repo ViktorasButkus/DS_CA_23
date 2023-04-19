d728436104e066504243b05b4b3303ed3345e058
ViktorasButkus <viktoras.butkus@live.com>
2023-04-19 07:36:56 +0100

    Service three remaining setup

--- src/main/java/serviceThreeClientAndServer/client/serviceThreeClient.java
+++ src/main/java/serviceThreeClientAndServer/client/serviceThreeClient.java
@@ -1,4 +1,87 @@
 package serviceThreeClientAndServer.client;
 
+import com.proto.serviceThree.*;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class serviceThreeClient {
+
+    //Unary
+    private static void doActivateSensors(ManagedChannel channel) {
+        System.out.println("Enter doActivateSensors");
+
+        SensorSystemServiceGrpc.SensorSystemServiceBlockingStub stub = SensorSystemServiceGrpc.newBlockingStub(channel);
+        ActivateSensorsResponse response = stub.activateSensors(ActivateSensorsRequest.newBuilder().setActivate(true).build());
+
+        System.out.println("Sensor message: " +response.getMessage());
+
+    }
+
+    //Client Streaming
+    private static void doDetectMotion(ManagedChannel channel) throws InterruptedException {
+        System.out.println("Enter doDetectMotion");
+
+        SensorSystemServiceGrpc.SensorSystemServiceStub stub = SensorSystemServiceGrpc.newStub(channel);
+
+        List<String> sensors = new ArrayList<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Collections.addAll(sensors, "1", "2", "3", "4");
+
+        StreamObserver<DetectMotionRequest> stream = stub.detectMotion(new StreamObserver<DetectMotionResponse>() {
+            @Override
+            public void onNext(DetectMotionResponse response) {
+                System.out.println(response.getAlertMessage());
+            }
+
+            @Override
+            public void onError(Throwable t) {}
+
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+
+            }
+        });
+
+        for (String sensor: sensors) {
+            stream.onNext(DetectMotionRequest.newBuilder().setSensorId(sensor).build());
+        }
+        stream.onCompleted();
+        latch.await(3, TimeUnit.SECONDS);
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+     if (args.length == 0) {
+        System.out.println("Need one argument to work");
+        return;
+    }
+
+    ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost", 50052)
+            .usePlaintext()
+            .build();
+
+        switch (args[0]) {
+        case "activateAlerts": doActivateSensors(channel); break;
+        case "streamAlerts": doDetectMotion(channel);break;
+        default:
+            System.out.println("keyword invalid- " + args[0]);
+    }
+
+
+        System.out.println("Shutting down");
+        channel.shutdown();
+    }
 }
--- src/main/java/serviceThreeClientAndServer/server/serviceThreeServerImpl.java
+++ src/main/java/serviceThreeClientAndServer/server/serviceThreeServerImpl.java
@@ -1,14 +1,41 @@
 package serviceThreeClientAndServer.server;
 
-import com.proto.serviceThree.SensorSystemServiceGrpc;
-import com.proto.serviceThree.ActivateSensorsRequest;
-import com.proto.serviceThree.ActivateSensorsResponse;
+import com.proto.serviceThree.*;
 import io.grpc.stub.StreamObserver;
 
 public class serviceThreeServerImpl extends SensorSystemServiceGrpc.SensorSystemServiceImplBase {
 
     @Override
     public void activateSensors(ActivateSensorsRequest request, StreamObserver<ActivateSensorsResponse> responseObserver){
+
+        responseObserver.onNext(ActivateSensorsResponse.newBuilder().setMessage("Sensor status " + request.getActivate()).build());
+        responseObserver.onCompleted();
+
+    }
+
+    @Override
+    public StreamObserver<DetectMotionRequest> detectMotion (StreamObserver<DetectMotionResponse> responseObserver){
+        StringBuilder sb = new StringBuilder();
+
+        return new StreamObserver<DetectMotionRequest>() {
+            @Override
+            public void onNext(DetectMotionRequest request) {
+                sb.append("Sensor providing information: ");
+                sb.append(request.getSensorId() + " " + request.getMotionDetected());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(DetectMotionResponse.newBuilder().setAlertMessage(sb.toString()).build());
+                responseObserver.onCompleted();
+            }
+        };
+
 
     }
 }
